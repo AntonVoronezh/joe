bbe2d3249fb2e41711d92bc8937f2b117be7bb7f
a.glushenkov <a.glushenkov@bi.zone>
2023-12-13 20:39:33 +0300

    пересен скрин в другое место

--- helpers/platforms/telemetr/mentioned_table/clicks/click_with_reposts.py
+++ helpers/platforms/telemetr/mentioned_table/clicks/click_with_reposts.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 def click_with_reposts(driver):
-    driver.execute_script("window.scrollTo(0, 700)")
+    driver.execute_script("window.scrollTo(0, 800)")
 
     element = driver.find_element(By.XPATH, '//div[@id="portlet_who_mentioned"]')
     element_2 = element.find_element(By.CSS_SELECTOR, 'label')
--- helpers/platforms/telemetr/mentioned_table/get_mentioned_table.py
+++ helpers/platforms/telemetr/mentioned_table/get_mentioned_table.py
@@ -11,7 +11,11 @@ from settings import is_telemetr_save_screenshot
 
 def get_mentioned_table(driver, result_out_path, file_name):
     click_with_reposts(driver=driver)
-    time.sleep(5)
+
+    if is_telemetr_save_screenshot:
+        save_screen_with_sign(driver=driver, result_out_path=result_out_path, file_name=file_name)
+
+    time.sleep(1)
 
     data_tables_div = driver.find_element(By.CSS_SELECTOR, 'table#who_mentioned')
     driver.execute_script("document.getElementById('who_mentioned').scrollIntoView();")
@@ -28,9 +32,6 @@ def get_mentioned_table(driver, result_out_path, file_name):
         driver.execute_script("arguments[0].scrollBy(0, arguments[0].scrollHeight);", data_tables_div)
         all_elements = driver.find_elements(By.CSS_SELECTOR, 'div.dataTables_scrollBody > table > tbody > tr')
         html = driver.page_source
-
-        if is_telemetr_save_screenshot:
-            save_screen_with_sign(driver=driver, result_out_path=result_out_path, file_name=file_name)
 
         soup_for_tr = BeautifulSoup(html, 'lxml')
 
